061cbbe3f72f5f11e71eddb5cfb15c7d9eff8fd2
boucp <130845503+boucp@users.noreply.github.com>
2023-05-02 01:28:31 +0600

    Update config.js

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -63,3 +63,8 @@ MathJax = {
 document$.subscribe(() => {
   MathJax.typesetPromise()
 })
+
+$( '.contributors img[data-src]' ).each( function() {
+        src = $(this).attr("data-src");
+        $(this).attr('src',src);
+    });
